cdcc9e952db4f27b6649c288245d010598a6b9cb
Michaell Joel reyes <michaelljoel.reyes202@gmail.com>
2022-01-17 18:08:12 -0600

    Michaell Reyes

---
+++ ProyectoPeliculas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoPeliculas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
